22de0b9c57de3c307b1e455f719525d1655daf80
NagaJun1 <74446955+NagaJun1@users.noreply.github.com>
2024-02-03 19:00:31 +0900

    google-charts を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Chart from 'react-google-charts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// インストール
+// npm install --save react-google-charts
+
+// 公式ドキュメント
+// https://www.react-google-charts.com/examples/pie-chart
+
+/** メイン画面 */
+const App = () => (
+  <div className="App">
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
+  </div>
+);
+
+/** 円グラフのオプション */
+export const options = {
+  title: "タイトル",
+};
+
+/** 円グラフに描写するデータ */
+const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 11],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7],
+];
 
 export default App;
