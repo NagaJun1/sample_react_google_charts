37d70f14274639cf0c50301a3e8813a588afae27
junpei.nagai <jumpei603@gmail.com>
2024-02-04 11:31:55 +0900

    棒グラフにイベント処理を追加

--- src/ts/BarChart.tsx
+++ src/ts/BarChart.tsx
@@ -1,32 +1,43 @@
 import Chart, { ReactGoogleChartEvent } from 'react-google-charts';
 import '../css/BarChart.css';
+import { useState } from 'react';
 
 const BarChart = () => {
+  const [selectedRow, setSelectedRow] = useState(0);
+  const [selectedColumn, setSelectedColumn] = useState(0);
+
+  /** 棒グラフのイベント */
+  const chartEvents: ReactGoogleChartEvent[] = [{
+    eventName: "select",
+    callback({ chartWrapper }) {
+      console.log(chartWrapper.getChart().getSelection())
+
+      // チャートの選択項目変更で、ラベルを書き換え
+      let select = chartWrapper.getChart().getSelection()[0];
+      setSelectedRow(Number(select.row));
+      setSelectedColumn(Number(select.column));
+    }
+  }];
+
   return (<>
-    <Chart
-      chartType="BarChart"
-      width="100%"
-      height="400px"
-      data={data}
-      options={options}
-      chartEvents={chartEvents}
-    />
+    <div className='flex'>
+      <div className='min_width150 margin30 padding5 bg_blue'>
+        <h3>selected Row</h3>
+        <h3>{selectedRow}</h3>
+        <h3>selected Column</h3>
+        <h3>{selectedColumn}</h3>
+      </div>
+      <Chart
+        chartType="BarChart"
+        width="100%"
+        height="400px"
+        data={data}
+        options={options}
+        chartEvents={chartEvents}
+      />
+    </div>
   </>);
 }
-
-/** チャートのイベント */
-const chartEvents: ReactGoogleChartEvent[] = [{
-  eventName: "select",
-  callback({ chartWrapper }) {
-    console.log(chartWrapper.getChart().getSelection())
-
-    // チャートの選択項目変更で、ラベルを書き換え
-    let selectedIndex = chartWrapper.getChart().getSelection()[0].row;
-    if (0 <= selectedIndex) {
-      console.log("selected : " + selectedIndex)
-    }
-  }
-}];
 
 /** グラフに表示するデータ */
 export const data = [
